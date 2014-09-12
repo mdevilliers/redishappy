49e036abc352ca0de823080b258d76f84f44abac
Mark deVilliers <markdevilliers@gmail.com>
2014-09-12 16:17:42 +0100

    added additional test for non existing templates.

--- readme.md
+++ readme.md
@@ -19,3 +19,13 @@ curl -X POST -H "Content-Type: application/json" \
 -d '{"method":"HelloService.Say","params":[{"Who":"Test"}], "id":"1"}' \
 http://localhost:8085/rpc
 ```
+
+```
+go test ./... -cover
+```
+
+
+To read
+-------
+
+https://medium.com/@Drew_Stokes/actual-zero-downtime-with-haproxy-18318578fde6
--- template/writer_test.go
+++ template/writer_test.go
@@ -16,9 +16,23 @@ func TestLoadTempate(t *testing.T) {
 	renderedTemplate, err := ExecuteTemplate( &proxy, &arr )
 
 	if err != nil{
-		t.Error("Error writing test file")
+		t.Error("Error rendering test file")
 	}
 
 	fmt.Printf("%s", renderedTemplate)
 }
 
+func TestLoadNonExistingTempate(t *testing.T) {
+
+	proxy := configuration.HAProxy{TemplatePath : "does_not_exist_template.cfg"}
+	master1 := MasterDetails{Name : "one", Ip : "10.0.0.1", Port : 2345, ExternalPort : 5432}
+
+	arr := []MasterDetails{master1}
+
+	_, err := ExecuteTemplate( &proxy, &arr )
+
+	if err == nil{
+		t.Error("Template doesn't exist - this should error")
+	}
+	
+}
