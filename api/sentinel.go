6e403035528674c81958cf4f4a49acd346fa31f4
Mark deVilliers <markdevilliers@gmail.com>
2014-09-19 14:02:24 +0000

    added an api to get the current sentinel state

---
+++ api/sentinel.go
@@ -0,0 +1,26 @@
+package api
+
+import(
+	"encoding/json"
+    "github.com/mdevilliers/redishappy/sentinel"
+	"net/http"
+)
+
+type SentinelApi struct {
+	Manager *sentinel.SentinelManager
+}
+
+func (s *SentinelApi)Get(w http.ResponseWriter, r *http.Request) {
+
+	responseChannel := make(chan sentinel.SentinelTopology)
+	s.Manager.GetState(sentinel.TopologyRequest{ReplyChannel: responseChannel})
+	topologyState := <-responseChannel
+	responseJSON(w, topologyState)
+}
+
+
+func responseJSON(w http.ResponseWriter, data interface {}) {
+	w.Header().Set("Content-Type", "application/json")
+	bites, _ := json.Marshal(data)
+	w.Write(bites)
+}
