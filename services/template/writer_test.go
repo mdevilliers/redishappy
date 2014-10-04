04e2776b3e17991d74f6e95cb7ebf5c68f0c8904
MarkDv <markdevilliers@gmail.com>
2014-10-04 22:31:03 +0100

    fixed up template test

--- services/template/writer_test.go
+++ services/template/writer_test.go
@@ -13,7 +13,7 @@ func TestLoadTempate(t *testing.T) {
 	collection.AddOrReplace(&types.MasterDetails{Name: "one", Ip: "10.0.0.1", Port: 2345, ExternalPort: 5432})
 	collection.AddOrReplace(&types.MasterDetails{Name: "two", Ip: "10.0.1.1", Port: 5432, ExternalPort: 2345})
 
-	renderedTemplate, err := RenderTemplate(path, collection)
+	renderedTemplate, err := RenderTemplate(path, &collection)
 
 	if err != nil {
 		t.Error("Error rendering test file")
@@ -27,7 +27,7 @@ func TestLoadNonExistingTempate(t *testing.T) {
 	path := "does_not_exist_template.cfg"
 	collection := types.NewMasterDetailsCollection()
 
-	_, err := RenderTemplate(path, collection)
+	_, err := RenderTemplate(path, &collection)
 
 	if err == nil {
 		t.Error("Template doesn't exist - this should error")
