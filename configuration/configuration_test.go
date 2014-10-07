d6a7e8dff37bd05bb55baed26e55832bf3e4fa49
MarkDv <markdevilliers@gmail.com>
2014-10-07 21:28:12 +0100

    additional tests for the sanity check

--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -78,34 +78,48 @@ func TestNonExistentFile(t *testing.T) {
 func TestSanityCheckBasicUsage(t *testing.T) {
 
 	logger.InitLogging("../log")
-	config := `{
-				  "Clusters" :[
-				  {
-				    "Name" : "cluster one",
-				    "MasterPort" : 6379
-				  },
-				  {
-				    "Name" : "cluster two",
-				    "MasterPort" : 6380
-				  }],
-				  "Sentinels" : [ 
-				      {"Host" : "192.168.0.20", "Port" : 26379},
-				      {"Host" : "192.168.0.21", "Port" : 26379}
-				  ]
-			}`
 
-	parsedconfig, _ := ParseConfiguration([]byte(config))
-	sane, errors := parsedconfig.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
+	clusters := []types.Cluster{types.Cluster{Name: "one", MasterPort: 1234}}
+	sentinels := []types.Sentinel{types.Sentinel{Host: "192.168.0.20", Port: 26379}}
+
+	config := &Configuration{Clusters: clusters, Sentinels: sentinels}
+
+	sane, errors := config.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
 
 	if !sane {
 		t.Errorf("This is a valid sanity checked configuration : %t, %d", sane, len(errors))
 	}
 
-	parsedconfig.Sentinels = []types.Sentinel{}
+	config.Sentinels = []types.Sentinel{}
 
-	sane, errors = parsedconfig.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
+	sane, errors = config.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
 
 	if sane {
 		t.Errorf("Configuration has no sentinels configured : %t, %d", sane, len(errors))
+	}
+
+	config.Sentinels = nil
+
+	sane, errors = config.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
+
+	if sane {
+		t.Errorf("Configuration has no sentinels configured : %t, %d", sane, len(errors))
+	}
+
+	config.Sentinels = sentinels
+	config.Clusters = []types.Cluster{}
+
+	sane, errors = config.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
+
+	if sane {
+		t.Errorf("Configuration has no clusters configured : %t, %d", sane, len(errors))
+	}
+
+	config.Clusters = nil
+
+	sane, errors = config.SanityCheckConfiguration(&ConfigContainsRequiredSections{})
+
+	if sane {
+		t.Errorf("Configuration has no clusters configured : %t, %d", sane, len(errors))
 	}
 }
